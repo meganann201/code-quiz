10875cba858d3c2ab5ac4c38a96a250382e55d11
Megan Campbell <megancampbell201@gmail.com>
2022-01-15 18:48:18 -0500

    function to submit answer

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,84 +1,84 @@
+// array holding questions and answers
 var questions = [
     {
     question: "Inside which HTML element do we put the JavaScript?",
     answers: {
      a: "<script>",
-     b: "<javascript",
-     c: "<js"
+     b: "<javascript>",
+     c: "<js>"
     },
      correctAnswer: "a"
-},
- {
-    question: "How do you write a function in JavaScript?", 
-    answers: {
-     a: "function:myFunction()",
-     b: "function = myFunction()",
-     c: "function myFunction()"
     },
-     correctAnswer: "c"
-    
-},
- {
-    question: "How do you call a function named 'myFunction'?",
-    answers: {
-    a: "myFunction()",  
-    b: "call myFunction()",
-    c: "call function myFunction()"
+    {
+        question: "How do you write a function in JavaScript?", 
+        answers: {
+        a: "function:myFunction()",
+        b: "function = myFunction()",
+        c: "function myFunction()"
+        },
+        correctAnswer: "c"
+        
     },
-    correctAnswer: "a"
-},
- {
-    question: "How can you add a comment in a JavaScript?",
-    answers: {
-    a: "'This is a comment",
-    b: "//This is a comment",
-    c: "<!--This is a comment-->"
+    {
+        question: "How do you call a function named 'myFunction'?",
+        answers: {
+        a: "myFunction()",  
+        b: "call myFunction()",
+        c: "call function myFunction()"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "b"
-},
- {
-    question: "What is the correct way to write a JavaScript array?",
-    answers: {
-    a: "var colors = 'red', 'green', 'blue'",
-    b: "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
-    c: "var colors = ['red', 'green', 'blue']"
+    {
+        question: "How can you add a comment in a JavaScript?",
+        answers: {
+        a: "'This is a comment",
+        b: "//This is a comment",
+        c: "<!--This is a comment-->"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "c"
-},
- {
-    question: "How do you round the number 7.25, to the nearest integer?",
-    answers: {
-    a: "round(7.25)",
-    b: "Math.round(7.25)", 
-    c: "Math.rnd(7.25)"
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        answers: {
+        a: "var colors = 'red', 'green', 'blue'",
+        b: "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+        c: "var colors = ['red', 'green', 'blue']"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "b"
-},
- {
-    question: "Which event occurs when the user clicks on an HTML element?",
-    answers: {
-    a: "onclick",
-    b: "onchange",
-    c: "onmouseclick"
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        answers: {
+        a: "round(7.25)",
+        b: "Math.round(7.25)", 
+        c: "Math.rnd(7.25)"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "a"
-},
- {
-    question: "How do you declare a JavaScript variable?",
-    answers: {
-    a: "v carName;",
-    b: "variable carName;",
-    c: "var carName;"
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        answers: {
+        a: "onclick",
+        b: "onchange",
+        c: "onmouseclick"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "c"
-},
+    {
+        question: "How do you declare a JavaScript variable?",
+        answers: {
+        a: "v carName;",
+        b: "variable carName;",
+        c: "var carName;"
+        },
+        correctAnswer: "c"
+    }
 ]
-
+var score = 0;
+var questionIndex = 0;
+//selecting the HTML elements
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-    
-    
 const startButton = document.getElementById('start-btn');
 
 startButton.addEventListener("click", startQuiz);
@@ -103,3 +103,20 @@ function showQuestion(index) {
     answer2.textContent = questions[index].answers.b
     answer3.textContent = questions[index].answers.c
 }
+
+function submitAnswer(button){
+    console.log(button.textContent);
+     // if else statement for if button clicked = correct answer 
+      var answerSelected = button.textContent
+      var correctAnswerKey = questions[questionIndex].correctAnswer
+      var correctAnswerValue = questions[questionIndex].answers[correctAnswerKey]
+      if (answerSelected === correctAnswerValue){
+          alert("correct");
+      } else {
+          //return false and show correct answer
+          alert("wrongo!");
+      }
+}
+
+  
+
