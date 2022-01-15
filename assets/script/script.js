2095d988811d7dd1d2a3b369a7235175819e7080
Megan Campbell <megancampbell201@gmail.com>
2022-01-15 14:47:54 -0500

    created variables to select HTML elements

--- assets/script/script.js
+++ assets/script/script.js
@@ -73,3 +73,9 @@ var questions = [
     correctAnswer: "c"
 },
 ]
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+    
+    
