6824fb9350d324a4f89f3b1018e3fb18fcb5b4a2
Megan Campbell <megancampbell201@gmail.com>
2022-01-15 11:44:59 -0500

    created a varibale of questions and answers

---
+++ assets/script/script.js
@@ -0,0 +1,75 @@
+var questions = [
+    {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: {
+     a: "<script>",
+     b: "<javascript",
+     c: "<js"
+    },
+     correctAnswer: "a"
+},
+ {
+    question: "How do you write a function in JavaScript?", 
+    answers: {
+     a: "function:myFunction()",
+     b: "function = myFunction()",
+     c: "function myFunction()"
+    },
+     correctAnswer: "c"
+    
+},
+ {
+    question: "How do you call a function named 'myFunction'?",
+    answers: {
+    a: "myFunction()",  
+    b: "call myFunction()",
+    c: "call function myFunction()"
+    },
+    correctAnswer: "a"
+},
+ {
+    question: "How can you add a comment in a JavaScript?",
+    answers: {
+    a: "'This is a comment",
+    b: "//This is a comment",
+    c: "<!--This is a comment-->"
+    },
+    correctAnswer: "b"
+},
+ {
+    question: "What is the correct way to write a JavaScript array?",
+    answers: {
+    a: "var colors = 'red', 'green', 'blue'",
+    b: "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+    c: "var colors = ['red', 'green', 'blue']"
+    },
+    correctAnswer: "c"
+},
+ {
+    question: "How do you round the number 7.25, to the nearest integer?",
+    answers: {
+    a: "round(7.25)",
+    b: "Math.round(7.25)", 
+    c: "Math.rnd(7.25)"
+    },
+    correctAnswer: "b"
+},
+ {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answers: {
+    a: "onclick",
+    b: "onchange",
+    c: "onmouseclick"
+    },
+    correctAnswer: "a"
+},
+ {
+    question: "How do you declare a JavaScript variable?",
+    answers: {
+    a: "v carName;",
+    b: "variable carName;",
+    c: "var carName;"
+    },
+    correctAnswer: "c"
+},
+]
